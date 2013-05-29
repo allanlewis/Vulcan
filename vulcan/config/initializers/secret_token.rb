a0c1a16ac0e318d6534c93028d7a6fd5a6dbec4f
Allan Lewis <allanlewis99@gmail.com>
2013-05-29 14:55:01 -0400

    create rails app

---
+++ vulcan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vulcan::Application.config.secret_token = '43c715df047bb8249278ca7d867e8f1e4c325d7159493649f4e271cc983dfcde8f92970b852933b4057a40c55f2704d696a3254e4e2b79e97a039d65b276869c'
